45fa8e53d933a3e27b3225b2073a0d9dc989a3ed
marsh000111 <65301716+marsh000111@users.noreply.github.com>
2022-06-19 21:41:13 -0400

    Working website, no frills. Version 1.0

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("decorations").onclick = function(){
+    location.href = 'decoration.html';
+}
+document.getElementById("home").onclick = function(){
+    location.href = 'index.html';
+}
